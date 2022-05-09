896812d18f05761cca32478aa5c27435d777fbae
Rafael de Amaral <rafael_ftec@hotmail.com>
2022-05-09 16:41:28 -0300

    Update Product Use case

---
+++ src/usecase/product/update/update.product.integration.spec.ts
@@ -0,0 +1,43 @@
+import { Sequelize } from "sequelize-typescript";
+import Product from "../../../domain/product/entity/product";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import UpdateProductUseCase from "./update.product.usecase";
+
+describe("Test update product use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should update a product", async () => {
+    const productRepository = new ProductRepository();
+    const usecase = new UpdateProductUseCase(productRepository);
+    const product = new Product("123", "Product Name", 1.0);
+
+    await productRepository.create(product);
+
+    const input = {
+      id: "123",
+      name: "Product Name Updated",
+      price: 2.0
+    };
+
+    const output = await usecase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+});
---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,34 @@
+import Product from "../../../domain/product/entity/product";
+import UpdateProductUseCase from "./update.product.usecase";
+
+const product = new Product(
+  "123",
+  "Product Name",
+  1.0
+);
+
+const input = {
+  id: product.id,
+  name: "Product Name Updated",
+  price: 2.0
+};
+
+const MockRepository = () => {
+  return {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(product)),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit test for product update use case", () => {
+  it("should update a product", async () => {
+    const productRepository = MockRepository();
+    const productUpdateUseCase = new UpdateProductUseCase(productRepository);
+
+    const output = await productUpdateUseCase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+});
